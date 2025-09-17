c6b6574445239c62012beb24caf6e1ce1796e41e
PresleyHe <prehe28@bergen.org>
2025-09-17 11:47:51 -0400

    Lesson 1 completed(Revised)

--- Lesson1-Conditionals/conditionals.py
+++ Lesson1-Conditionals/conditionals.py
@@ -6,14 +6,14 @@
 # ========================================
 # SECTION 1: QUICK TRANSLATION CHALLENGE
 # ========================================
-print("==Grade Classification==")
-score = 87
-if score >= 90:
-    print("A grade!")
-elif score >= 80:
-    print("B grade!")
-else:
-    print("BELOW B")
+# print("==Grade Classification==")
+# score = 87
+# if score >= 90:
+#     print("A grade!")
+# elif score >= 80:
+#     print("B grade!")
+# else:
+#     print("BELOW B")
 
 
 # ========================================
@@ -21,14 +21,14 @@ else:
 # ========================================
     
 
-age_input = input("Enter your age: ")
+age_input = int(input("Enter your age: "))
 # age = int(age_input)
 if age_input :
-    if 12 >= age_input >= 0:
+    if 0 <= age_input <= 12:
         print("Your are a child!")
-    elif 13 >= age_input >=10 :
+    elif 13 <= age_input <=19 :
         print("You are a teenager") 
-    elif 20 >= age_input >=64 :
+    elif 20 <= age_input <=64 :
         print("You are an adult") 
     elif age_input >=65:
         print("You are a Senior")
