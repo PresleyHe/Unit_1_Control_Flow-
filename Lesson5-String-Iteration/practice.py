96f831a26ed7c08e48768c75ce54f551bc00adeb
PresleyHe <prehe28@bergen.org>
2025-09-29 08:58:15 -0400

    Lesson5 text analysis complete

---
+++ Lesson5-String-Iteration/practice.py
@@ -0,0 +1,18 @@
+# count = 0
+# text = "Hello world"
+# vowels = "aeiouAEIOU"
+# for char in text:
+#     if char in vowels:
+#         count +=1
+# print(f"Volwels in {text}: {count}")
+
+
+# new_text = "ABC123xyz"
+# for i in range(len(new_text)):
+#     if '0' <= new_text[i] <= '9':
+#         print(f"Digit at position {i}: {new_text[i]}")
+        
+# word = "Hello"
+# for i in range(len(word)):
+#     print(f"{word[i]} at index {i} and {word[-1-i]} at index {-1-i}")
+        
