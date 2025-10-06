cc4b7a446aaec35f15ea1223d7026668a443eb3e
PresleyHe <prehe28@bergen.org>
2025-10-06 08:56:36 -0400

    completed

---
+++ Lesson6-While-loops/part2-else/validation.py
@@ -0,0 +1,21 @@
+# Input Validation 1 - Username validation
+while True:
+    username = input("Enter a username (3-15 chars):")
+    if len(username) < 3:
+        print("Too short! Min 3 characters!")
+        continue
+    if len(username) > 15:
+        print("Too long! Max 15 characters!")
+        continue
+    #Check if username has any space
+    has_space = False
+    for char in username:
+        if char == " ":
+            has_space = True
+            break
+    if has_space:
+        print("No spaces allowed!")
+        continue
+    # username validation passed
+    print(f"Username: '{username}' accepted! \n")
+    break
